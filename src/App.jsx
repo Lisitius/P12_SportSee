9b2c8d990cf57296f71476d0a3d92ed4a6a14706
unknown <benjamin.fichaux@gmail.com>
2023-05-11 18:24:29 +0200

    first commit + sass setup 7-1 + mock api

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./sass/pages/_app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <p>HELLO WORLD</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+const routeFile = () => {
+  return (
+    <Routes>
+      {/* Route */}
+      {/* Route */}
+      {/* Route */}
+    </Routes>
+  );
+};
